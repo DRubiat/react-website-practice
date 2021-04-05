acb77e4018f422d40fa139092602d3d47a8b7f46
Daneillan Rubiat <danrubiat@gmail.com>
2021-04-05 17:55:21 +0100

    Finished version

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import CardItem from "./CardItem";
 import './Cards.css';
+import Waterfall from "../assets/images/img-9.jpg"
+import Cruise from "../assets/images/img-2.jpg"
+import Sail from "../assets/images/img-3.jpg"
+import Himalayas from "../assets/images/img-4.jpg"
+import Sahara from "../assets/images/img-8.jpg"
 
 function Cards() {
     return (
@@ -10,13 +15,13 @@ function Cards() {
                 <div className="cards_wrapper">
                     <ul className='cards_items'>
                         <CardItem
-                            src='images/img-9.jpg'
+                            src={Waterfall}
                             text='Explore the hidden waterfall deep inside the Amazon Jungle'
                             label='Adventure'
                             path='/services'
                         />
                         <CardItem
-                            src='images/img-2.jpg'
+                            src={Cruise}
                             text='Travel through the Islands of Bali in a Private Cruise'
                             label='Luxury'
                             path='/services'
@@ -24,19 +29,19 @@ function Cards() {
                     </ul>
                     <ul className='cards_items'>
                         <CardItem
-                            src='images/img-3.jpg'
+                            src={Sail}
                             text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
                             label='Mystery'
                             path='/services'
                         />
                         <CardItem
-                            src='images/img-4.jpg'
+                            src={Himalayas}
                             text='Experience Football on Top of the Himalayan Mountains'
                             label='Adrenaline'
                             path='/products'
                         />
                         <CardItem
-                            src='images/img-8.jpg'
+                            src={Sahara}
                             text='Ride through the Sahara Desert on a guided camel tour'
                             label='Explore'
                             path='/sign-up'
